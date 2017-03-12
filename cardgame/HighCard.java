40fe8bcac5b933ff8a22b79e6394a3e7708713c5
Rob <robflettcc@gmail.com>
2017-03-12 16:59:51 +0000

    interface between Rules and HighCard added

---
+++ cardgame/HighCard.java
@@ -0,0 +1,16 @@
+package cardgame;
+
+
+class HighCard implements Rules {
+
+  // private String howToPlay;
+
+// public HighCard(String howToPlay){
+//   this.howToPlay = howToPlay;
+// }
+
+public String returnRules(){
+  return "Play a card - whoever gets the highest value wins!";
+}
+
+}
--- cardgame/Rules.java
+++ cardgame/Rules.java
@@ -1,2 +1,8 @@
 // a decision maker interface for the different card rules to plug into
 package cardgame;
+
+public interface Rules{
+
+  public String returnRules();
+  
+}
